61fdadc0d540f47cf03540e614ddbeb0b59251eb
weychan <weychen@163.com>
2015-04-17 18:55:14 +0800

    asd

--- Application/Api/Controller/UserController.class.php
+++ Application/Api/Controller/UserController.class.php
@@ -10,6 +10,8 @@ namespace Api\Controller;
 use Api\Model\UsersModel;
 use Think\Controller\RestController;
 
+require_once MODULE_PATH. "aliyun-php/aliyun.php";
+use \Aliyun\OSS\OSSClient;
 class UserController extends RestController {
 
     protected $userFields = 'id,mobile,password,name,avatar,isValid,created_at,updated_at';
@@ -107,12 +109,12 @@ class UserController extends RestController {
         $token = I('post.token');
         $condition['token'] = $token;
         $tokenData = M('tokens')->field('userType,user_id')
-            ->where($condition)->select();
-
-        $data = M('users')->field('id,name,avatar')
-            ->where(array('id' => $tokenData[user_id]))->select();
-
-        $this->response($data,'json');
+            ->where($condition)->select()[0];
+        $data = M('users')->field('id, name, avatar')
+            ->where(array('id' => $tokenData['user_id']))->select();
+        $result['status'] = 'OK';
+        $result['content'] = $data;
+        $this->response($result,'json');
     }
 
     public function bindJPushRegistrationID()
@@ -354,6 +356,10 @@ class UserController extends RestController {
         $Order = M('transport_orders');
         $Order->where($condition)->save($data);
         if($Order){
+            $driver_id = $Order->where($condition)->getField('driver_id'); //获得司机id
+            $driver_data['isFree'] = 1;         //将isFree 设置成1
+            $condition2['id'] = $driver_id;     //查询司机的条件
+            M('driver')->where($condition2)->save($driver_data);
             $result['status'] = 'OK';
             $this->response($result,'json');
         }
@@ -520,5 +526,33 @@ class UserController extends RestController {
         $s = $s * 6378.137;
         $s = round($s * 10000) / 10000;
         return $s;
+    }
+
+    public function update_pic()
+    {
+//        echo MODULE_PATH. "aliyun-php/aliyun.php" ;
+        $avatar = I('post.avatar');
+        $client = OSSClient::factory(array(
+            'AccessKeyId' => 'PdUWUlXoZ0iS05hF',
+            'AccessKeySecret' => 'nsMLg5QRScXirbW6UGL9Ec6VGqP2VV',
+        ));
+//        $client->setBucketAcl(array(
+//            'Bucket' => 'banar-image2',
+//            'ACL' => 'public-read',
+//        ));
+        $client->putObject(array(
+            'Bucket' => 'banar-image2',
+            'Key' => '123',
+            'Content' => $avatar,
+        ));
+
+//        $objectListing = $client->listObjects(array(
+//            'Bucket' => 'banar-image',
+//        ));
+//
+//        foreach ($objectListing->getObjectSummarys() as $objectSummary) {
+//            echo $objectSummary->getKey();
+//        }
+
     }
 }
