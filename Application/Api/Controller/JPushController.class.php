f09de11fe7775cd9a375ffbd6f9cb80377d600fe
newway1995 <nniuwei@163.com>
2015-04-17 16:58:04 +0800

    加入了极光推送,牛威

---
+++ Application/Api/Controller/JPushController.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niuwei
+ * Date: 15/4/17
+ * Time: 14:36
+ */
+
+namespace Api\Controller;
+use Think\Controller\RestController;
+
+require_once '../../vendor/autoload.php';
+
+use JPush\Model as M;
+use JPush\JPushClient;
+
+class JPushController extends RestController{
+    //商户appKay
+    public $merchant_appKey = "868cb71e4bf509eceb859d20";
+    public $merchant_secret = "d21e8793170e4abdecec17fc";
+    //司机appKey
+    public $driver_appKey = "52850fe9e445eaf69bbb1b13";
+    public $driver_secret = "79dfe4dd25a796b74fbb4af8";
+
+    /**
+     * 将消息发送到指定的商户
+     * @param $registration_id
+     * @param $content
+     */
+    public function sendToMerchantByRegistrationID($registration_id, $content) {
+        $client = new JPushClient($this->$merchant_appKey, $this->$merchant_secret);
+        $response = $client->push()->setPlatform(M\all)
+            ->setAudience(M\audience(M\registration_id(array($registration_id))))
+            ->setNotification(M\notification($content))
+            ->send();
+
+        $this->assertTrue($response->isOk === true);
+    }
+
+    /**
+     * 将消息发送到指定的司机
+     * @param $registration_id
+     * @param $content
+     */
+    public function sendToDriverByRegistrationID($registration_id, $content) {
+        $client = new JPushClient($this->driver_appKey, $this->driver_secret);
+        $response = $client->push()->setPlatform(M\all)
+            ->setAudience(M\audience(M\registration_id(array($registration_id))))
+            ->setNotification(M\notification($content))
+            ->send();
+
+        $this->assertTrue($response->isOk === true);
+    }
+}
